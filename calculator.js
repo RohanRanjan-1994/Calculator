a30977d7e850b9016381cece483cedd626315713
Rohan Ranjan <rohanranjanghosh@gmail.com>
2020-11-18 11:55:11 +0530

    Calculator

---
+++ calculator.js
@@ -0,0 +1,38 @@
+// requiring modules
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res){
+    console.log(req.body);
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+    var result = num1 + num2;
+    res.send("The result " + result);
+});
+
+app.get("/bmicalculator", function(req, res){
+   res.sendFile(__dirname + "/bmiCalculator.html"); 
+});
+
+app.post("/bmicalculator", function(req, res){
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+    var bmi = weight / (height*height);
+    res.send("Your BMI is "+ bmi);
+});
+
+
+
+
+app.listen(port, () =>{
+    console.log(`Example app listening at http://localhost:${port}`);
+});
